5c54e6c35c91ee240b6896b9df2f467933681143
John Doe <nadia643@gmail.com>
2020-10-11 13:41:08 +0100

    made a for loop for length of array of numbers

--- app.js
+++ app.js
@@ -1,11 +1,28 @@
 window.onload = function() {
 
 
-const calcButtons = document.getElementById("calcButtons")
+const calcButtons = document.getElementsByTagName("button")
 let values = document.getElementsByClassName("item").innerHTML
 
-calcButtons.addEventListener("click", function() {
-  console.log("hello world" + values);
+// calcButtons.addEventListener("click", function() {
+//   console.log("hello world" + values);
   
-})
+// })
+
+for(let i=0; i<calcButtons.length; i++) {
+  if(calcButtons[i].matches('item-1')) {
+    console.log("the " + calcButtons[i].textContent + ' is a number');
+    
+  }
+}
+}
+
+
+
+var birds = document.getElementsByTagName('li');
+
+for (var i = 0; i < birds.length; i++) {
+  if (birds[i].matches('.endangered')) {
+    console.log('The ' + birds[i].textContent + ' is endangered!');
+  }
 }
